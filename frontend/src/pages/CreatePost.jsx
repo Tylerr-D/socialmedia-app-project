2fc6cb12e6ba361189f300b396225f1f80bb7be4
tylermishra76-sudo <tylermishra76@gmail.com>
2026-05-17 13:44:39 +0530

    Update API URL to use environment variable

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -10,7 +10,7 @@ const CreatePost = () => {
 
     const formData = new FormData(e.target)
 
-   axios.post(`${getApiUrl()}/posts`, formData)
+axios.post(`${import.meta.env.VITE_API_URL}/posts`, formData)
       .then(() => {
         alert("Post created ")
         navigate('/feed')
